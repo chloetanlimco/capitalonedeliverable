9be6305429d7e37f1bad61b3806792317e4dfb56
Chloe Tanlimco <tanlimco@usc.edu>
2019-05-18 01:07:22 -0400

    Google Cloud compatibility

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import urllib.request, json
+# import urllib.request, json
 from flask import Flask, render_template
 
-# Configure API request
-endpoint = "https://developer.nps.gov/api/v1/parks?stateCode=me"
-HEADERS = {"Authorization":"9vkQK0bSPBKpWJs57opGQYCMaINTJmHqe1vmCAdy"}
-req = urllib.request.Request(endpoint,headers=HEADERS)
+# # Configure API request
+# endpoint = "https://developer.nps.gov/api/v1/parks?stateCode=me"
+# HEADERS = {"Authorization":"9vkQK0bSPBKpWJs57opGQYCMaINTJmHqe1vmCAdy"}
+# req = urllib.request.Request(endpoint,headers=HEADERS)
 
-
+# Initialize Application
 app = Flask(__name__)
 @app.route("/")
 def home():
