3c8a6341635f8375169a3f3f32f03df9e856e475
Chloe Tanlimco <tanlimco@usc.edu>
2019-05-22 23:46:29 -0700

    search page updated somewhat

--- main.py
+++ main.py
@@ -12,13 +12,27 @@ app = Flask(__name__)
 def home():
 
     if request.method == 'POST':
-        return postjson()
+        return allparks()
 
     return render_template("home.html")
 
-@app.route("/allparks")
+@app.route('/allparks', methods = ['GET', 'POST'])
 def allparks():
-    return render_template("filter.html")
+    # Executing and search (not or); not necessary to parse string
+    search = request.form["searchterms"]
+
+    # Initialize page-specific params for request
+    params = {"api_key": config.api_key, "q":str(search)}
+    try:
+        empDict = requests.get(endpoint,params=params,headers=header_)
+    except Exception as e:
+        print (e)
+
+    data = empDict.json()
+    print (type(data["data"]))
+    print (data["data"][0]["states"])
+    # return jsonify(data)
+    return render_template("filter.html", numentries=data["total"], value=data["data"])
 
 @app.route("/about")
 def about():
