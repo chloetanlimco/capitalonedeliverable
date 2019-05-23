81851aac32c65f827330321fdab0098ab4516bfa
Chloe Tanlimco <tanlimco@usc.edu>
2019-05-23 10:24:08 -0700

    edited css, added default advanced search page

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ from flask import Flask, render_template, request, json, jsonify
 import requests
 import config
 
+#helper function
+def stringify(states_array):
+    retval = ""
+    for x in states_array:
+        retval += x
+        retval += ","
+    retval = retval[:-1]
+    return retval
+
 # Initialize API Request
 endpoint = "https://developer.nps.gov/api/v1/parks?"
 header_ = {"Authorization": config.api_key, "content-type":"application/json"}
@@ -12,27 +21,42 @@ app = Flask(__name__)
 def home():
 
     if request.method == 'POST':
-        return allparks()
+        return advancedsearch()
 
     return render_template("home.html")
 
-@app.route('/allparks', methods = ['GET', 'POST'])
-def allparks():
-    # Executing and search (not or); not necessary to parse string
-    search = request.form["searchterms"]
+@app.route('/advancedsearch', methods = ['GET', 'POST'])
+def advancedsearch():
 
-    # Initialize page-specific params for request
-    params = {"api_key": config.api_key, "q":str(search)}
-    try:
+    all_states = ["AL","AK","AZ",'AR',"CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME",
+    "MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD",
+    "TN","TX","UT","VT","VA","WA","WV","WI","WY"]
+    curr_states = []
+
+    if request.method == 'POST':
+        # Executing and search (not or); not necessary to parse string
+        search = request.form["searchterms"]
+
+        # Initialize page-specific params for request
+        params = {"api_key": config.api_key, "q":str(search)}
+        try:
+            empDict = requests.get(endpoint,params=params,headers=header_)
+        except Exception as e:
+            print (e)
+
+        data = empDict.json()
+        print (type(data["data"]))
+        print (data["data"][0]["states"])
+        # return jsonify(data)
+        return render_template("filter.html", numentries=data["total"], value=data["data"])
+
+    else:
+        params = {"api_key": config.api_key}
         empDict = requests.get(endpoint,params=params,headers=header_)
-    except Exception as e:
-        print (e)
-
-    data = empDict.json()
-    print (type(data["data"]))
-    print (data["data"][0]["states"])
-    # return jsonify(data)
-    return render_template("filter.html", numentries=data["total"], value=data["data"])
+        data = empDict.json()
+        print (stringify(all_states))
+        # return jsonify(data)
+        return render_template("filter.html", numentries=data["total"], value=data["data"])
 
 @app.route("/about")
 def about():
@@ -56,3 +80,5 @@ def postJsonHandler():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
