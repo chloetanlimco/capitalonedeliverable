3fbe489e4091e29b76a670ff0aefe97c99013085
Chloe Tanlimco <tanlimco@usc.edu>
2019-05-28 00:09:34 -0700

    added comments

--- main.py
+++ main.py
@@ -216,5 +216,3 @@ def park(park_code):
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
--- static/js/advsearch.js
+++ static/js/advsearch.js
@@ -1,3 +1,4 @@
+// Creating menu (states)
 var j = 0;
 for (i = 0; i < 56; i++) {
 
@@ -39,6 +40,7 @@ for (i = 0; i < 56; i++) {
     element.appendChild(listitem);
 }
 
+// figure out which states are selected
 function determineSelected() {
     var curr_states = new Array();
     for (i = 0; i < 56; i++)
@@ -51,6 +53,7 @@ function determineSelected() {
     return curr_states;
 }
 
+// reload advancedsearch with appropriate filters
 function reloadUpdated() {
     var search = $('#searchbar').val();
     $.ajax({
@@ -61,61 +64,4 @@ function reloadUpdated() {
             document.write(response);
         }
     });
-}
-
-function redirect_to_park(park_code)
-{
-    document.location.href='{{ url_for( 'park', park_code=park_code) }}';
-    var parkurl = "park/" + park_code;
-    return parkurl;
-}
-
-for (i = 0; i < numentries; i++) {
-    var img = document.createElement("IMG");
-    img.setAttribute("src", String(imgArray[i]));
-    img.id = "icon";
-
-    var panel = document.createElement("div");
-    var park_code = data[i]["parkCode"];
-    panel.id = park_code;
-    panel.className = "resultblock";
-    panel.setAttribute("action", "/about");
-    panel.setAttribute("method", "post");
-    // creates unique link and redirects users
-    panel.setAttribute("onclick", "window.location.href=redirect_to_park(this.id)");
-
-    // title: full name of park
-    var text = document.createElement("h5");
-    var node = document.createTextNode(data[i]['fullName']);
-    text.appendChild(node);
-    text.id = "desblock";
-
-    // states + designations
-    var info = document.createElement("p");
-    var state_title = document.createTextNode("States: ");
-    var state = document.createTextNode(data[i]['states']);
-    var enter = document.createElement("br");
-    var designation_title = document.createTextNode("Designation: ");
-    var designation = document.createTextNode(data[i]['designation']);
-    info.appendChild(state_title);
-    info.appendChild(state);
-    info.appendChild(enter);
-    info.appendChild(designation_title);
-    info.appendChild(designation);
-    info.id = "desblock";
-
-    // description
-    var descriptionblock = document.createElement("p");
-    var des = document.createTextNode(data[i]['description']);
-    descriptionblock.id = "desblock";
-    descriptionblock.append(des);
-
-    // add to panel div
-    panel.append(img);
-    panel.append(text);
-    panel.append(info);
-    panel.append(descriptionblock);
-
-    var element = document.getElementById("maincontent");
-    element.appendChild(panel);
 }
